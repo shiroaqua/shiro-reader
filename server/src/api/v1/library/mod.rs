bf1ababf39fdbbb73d6b960fa73ec33017741a7b
shiroaqua <aquayume@outlook.com>
2026-05-13 22:12:33 +0800

    Pluralize API endpoint names

--- server/src/api/v1/library/mod.rs
+++ server/src/api/v1/library/mod.rs
@@ -8,17 +8,17 @@ pub mod bookshelf;
 
 pub fn router() -> Router<AppState> {
     Router::new()
-        .route("/bookshelf", post(bookshelf::handlers::create_bookshelf))
+        .route("/bookshelves", post(bookshelf::handlers::create_bookshelf))
         .route(
-            "/bookshelf/{bookshelf_id}",
+            "/bookshelves/{bookshelf_id}",
             delete(bookshelf::handlers::delete_bookshelf),
         )
         .route(
-            "/bookshelf/{bookshelf_id}/folder",
+            "/bookshelves/{bookshelf_id}/folders",
             post(bookshelf::folder::handlers::create_folder),
         )
         .route(
-            "/bookshelf/{bookshelf_id}/folder/{folder_id}",
+            "/bookshelves/{bookshelf_id}/folders/{folder_id}",
             delete(bookshelf::folder::handlers::delete_folder),
         )
 }
